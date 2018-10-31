63256f73b067c50b5ceaad448662926b2fec5b74
Gregor <gregor@martynus.net>
2018-10-31 10:27:11 -0700

    test: handle `pull_request.synchronize` action

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,8 @@
 const nock = require('nock')
 const markdownify = require('..')
 const { Probot } = require('probot')
-const payload = require('./fixtures/pull_request.opened.json')
+const payloadOpened = require('./fixtures/pull_request.opened.json')
+const payloadSynchronize = require('./fixtures/pull_request.synchronize.json')
 const prFiles = require('./fixtures/prFiles.json')
 const payloadFail = require('./fixtures/payloadFail.json')
 const prFilesFail = require('./fixtures/prFilesFail.json')
@@ -31,7 +32,7 @@ describe('markdownify', () => {
       })
       .reply(200)
 
-    await probot.receive({name: 'pull_request', payload})
+    await probot.receive({name: 'pull_request', payload: payloadOpened})
   })
 
   test('does not edit the pull request when no markdown files are editted', async () => {
@@ -42,5 +43,20 @@ describe('markdownify', () => {
       .reply(200, prFilesFail.data)
 
     await probot.receive({name: 'pull_request', payload: payloadFail})
+  })
+
+  test('edits the pull request when on "synchornize" action', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/321696/access_tokens')
+      .reply(200, {token: 'test'})
+      .get('/repos/hiimbex/testing-things/pulls/112/files')
+      .reply(200, prFiles.data)
+      .patch('/repos/hiimbex/testing-things/pulls/112', (body) => {
+        expect(body).toMatchObject({body: '\n\n-----\n[View rendered README.md](https://github.com/hiimbex/testing-things/blob/hiimbex-patch-41/README.md)'})
+        return true
+      })
+      .reply(200)
+
+    await probot.receive({name: 'pull_request', payload: payloadSynchronize})
   })
 })
